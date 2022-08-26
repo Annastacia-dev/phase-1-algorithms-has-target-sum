5fff3209ad6513b977e53c6d95b98accf3352909
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-08-26 16:07:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  let map = new Map();
+  for (let i=0; i<array.length; i++){
+    let num1 = array[i]
+    let num2 = target - num1;
+    if(map.has(num2)){
+      return true;
+    }
+
+    map.set(num1, i)
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n)- Linear time complexity - the algorithm loops through each number
 */
 
 /* 
   Add your pseudocode here
+
+  //Given an array [3, 6, 9, 11, 4] and target = 14
+  // Find two numbers in the array that add up to 14
+  // 3 located at index 0 and 11 at index 3 add up to 14
+  // 14-3 = 11
+  // Loop through the array to look for 11
+
 */
 
 /*
   Add written explanation of your solution here
+  // Use a hashmap to store the array as values to be compared to later
+  // Loop through the array to check if the complement which is target - array[i] is in the map
+  // Return true if it is, if not add the number and return false
 */
 
 // You can run `node index.js` to view these console logs
